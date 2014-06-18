3275c3a7eb397052e8bd0ebde1ffe67a0a621b10
tylerl0706 <tleonhardt@Tylers-MacBook-Pro.local>
2014-06-18 13:00:59 -0400

    latest tweet is displayed on refresh

--- index.js
+++ index.js
@@ -1,14 +1,35 @@
 // web.js
 var express = require("express"),
     logfmt = require("logfmt"),
-    app = express();
+    app = express(),
+    str = "tweet";
 
 
 
 app.use(logfmt.requestLogger());
 
+app.set("tweet", str);
+
 app.all('/', function (req, res) {
-    res.send('Tyler Loves Isabella!');
+
+    var twitter = require('ntwitter');
+
+    var twit = new twitter({
+        consumer_key: '3qh7f8nrRL722Ij71A8Qbgy3m',
+        consumer_secret: 'mgpJNZfwfe96NyPh4kt0kxVBu929aa6eYHoOTuFXOv7WAfobcp',
+        access_token_key: '2571622638-ZcwPHGZfgqq9C0YxLD3NXX1osSJwRALPJR4CCYK',
+        access_token_secret: 've3flKS441AJagYJ4vIqiJyn9Fbtels8j5LHLasYPM6XD'
+    });
+
+    twit.stream('statuses/filter', {
+        'track': '#testingsomething123'
+    }, function (stream) {
+        stream.on('data', function (data) {
+            console.log(data.user.name + "\n" + data.text);
+            str = data.user.name + "\n" + data.text;
+        });
+    });
+    res.send(str);
 });
 
 var port = Number(process.env.PORT || 5000);
@@ -16,20 +37,21 @@ app.listen(port, function () {
     console.log("Listening on " + port);
 });
 
-var twitter = require('ntwitter');
 
-var twit = new twitter({
-  consumer_key: '3qh7f8nrRL722Ij71A8Qbgy3m',
-  consumer_secret: 'mgpJNZfwfe96NyPh4kt0kxVBu929aa6eYHoOTuFXOv7WAfobcp',
-  access_token_key: '2571622638-ZcwPHGZfgqq9C0YxLD3NXX1osSJwRALPJR4CCYK',
-  access_token_secret: 've3flKS441AJagYJ4vIqiJyn9Fbtels8j5LHLasYPM6XD'
-});
+//PREVIOUS VERSION FOR REF
 
-twit.stream('statuses/filter', {'track':'#testingsomething123'}, function(stream) {
-  stream.on('data', function (data) {
-    console.log(data.user.name + "\n" + data.text);
-    app.use(function(req, res, next){
-        res.send(data.user.name + "\n" + data.text);
-    });
-  });
-});
+//var twitter = require('ntwitter');
+//
+//var twit = new twitter({
+//  consumer_key: '3qh7f8nrRL722Ij71A8Qbgy3m',
+//  consumer_secret: 'mgpJNZfwfe96NyPh4kt0kxVBu929aa6eYHoOTuFXOv7WAfobcp',
+//  access_token_key: '2571622638-ZcwPHGZfgqq9C0YxLD3NXX1osSJwRALPJR4CCYK',
+//  access_token_secret: 've3flKS441AJagYJ4vIqiJyn9Fbtels8j5LHLasYPM6XD'
+//});
+//
+//twit.stream('statuses/filter', {'track':'#testingsomething123'}, function(stream) {
+//  stream.on('data', function (data) {
+//    console.log(data.user.name + "\n" + data.text);
+//    str = data.user.name + "\n" + data.text;
+//  });
+//});
