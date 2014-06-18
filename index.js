413ae9bc11c0e15793e22502d4f0a8415c4b875a
tylerl0706 <tleonhardt@Tylers-MacBook-Pro.local>
2014-06-18 02:08:23 -0400

    testing heroku

--- index.js
+++ index.js
@@ -14,14 +14,14 @@ app.listen(port, function () {
     console.log("Listening on " + port);
 });
 
-var twitter = require('ntwitter');
-
-var twit = new twitter({
-  consumer_key: '3qh7f8nrRL722Ij71A8Qbgy3m',
-  consumer_secret: 'mgpJNZfwfe96NyPh4kt0kxVBu929aa6eYHoOTuFXOv7WAfobcp',
-  access_token_key: '2571622638-ZcwPHGZfgqq9C0YxLD3NXX1osSJwRALPJR4CCYK',
-  access_token_secret: 've3flKS441AJagYJ4vIqiJyn9Fbtels8j5LHLasYPM6XD'
-});
+//var twitter = require('ntwitter');
+//
+//var twit = new twitter({
+//  consumer_key: '3qh7f8nrRL722Ij71A8Qbgy3m',
+//  consumer_secret: 'mgpJNZfwfe96NyPh4kt0kxVBu929aa6eYHoOTuFXOv7WAfobcp',
+//  access_token_key: '2571622638-ZcwPHGZfgqq9C0YxLD3NXX1osSJwRALPJR4CCYK',
+//  access_token_secret: 've3flKS441AJagYJ4vIqiJyn9Fbtels8j5LHLasYPM6XD'
+//});
 
 //twit.stream('statuses/filter', {'track':'#testingsomething123'}, function(stream) {
 //  stream.on('data', function (data) {
