1c08e616202451ffc5e852a93a8d58d0e649f3fa
tylerl0706 <tleonhardt@Tylers-MacBook-Pro.local>
2014-06-18 01:47:30 -0400

    installed ntwitter

--- index.js
+++ index.js
@@ -1,15 +1,30 @@
 // web.js
-var express = require("express");
-var logfmt = require("logfmt");
-var app = express();
+var express = require("express"),
+    logfmt = require("logfmt"),
+    app = express();
 
 app.use(logfmt.requestLogger());
 
-app.get('/', function(req, res) {
-  res.send('Tyler Loves Isabella!');
+app.get('/', function (req, res) {
+    res.send('Tyler Loves Isabella!');
 });
 
 var port = Number(process.env.PORT || 5000);
-app.listen(port, function() {
-  console.log("Listening on " + port);
+app.listen(port, function () {
+    console.log("Listening on " + port);
+});
+
+var twitter = require('ntwitter');
+
+var twit = new twitter({
+  consumer_key: '3qh7f8nrRL722Ij71A8Qbgy3m',
+  consumer_secret: 'mgpJNZfwfe96NyPh4kt0kxVBu929aa6eYHoOTuFXOv7WAfobcp',
+  access_token_key: '2571622638-ZcwPHGZfgqq9C0YxLD3NXX1osSJwRALPJR4CCYK',
+  access_token_secret: 've3flKS441AJagYJ4vIqiJyn9Fbtels8j5LHLasYPM6XD'
+});
+
+twit.stream('statuses/filter', {'locations':'-122.75,36.8,-121.75,37.8,-74,40,-73,41'}, function(stream) {
+  stream.on('data', function (data) {
+    console.log(data);
+  });
 });
