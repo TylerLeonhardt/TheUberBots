a9a2f3e4998edea0ff45b4b03e9e4c1beadef5c3
tylerl0706 <tleonhardt@Tylers-MacBook-Pro.local>
2014-07-14 18:17:03 -0700

    tweets
    :wq

--- index.js
+++ index.js
@@ -2,29 +2,94 @@
 var express = require("express"),
     logfmt = require("logfmt"),
     app = express(),
-    str = "tweet";
+    str = "1+1 ",
+    Client = require('node-wolfram'),
+    Wolfram = new Client('4QQH9G-K8A2R2WAL3');
 
-var twitter = require('ntwitter');
+//var twitter = require('ntwitter');
+var twitter = require('twit');
 
-    var twit = new twitter({
-        consumer_key: '3qh7f8nrRL722Ij71A8Qbgy3m',
-        consumer_secret: 'mgpJNZfwfe96NyPh4kt0kxVBu929aa6eYHoOTuFXOv7WAfobcp',
-        access_token_key: '2571622638-ZcwPHGZfgqq9C0YxLD3NXX1osSJwRALPJR4CCYK',
-        access_token_secret: 've3flKS441AJagYJ4vIqiJyn9Fbtels8j5LHLasYPM6XD'
+var twit = new twitter({
+    consumer_key: 'Df9w73YbAhP3NFz1dVYDWIig9',
+    consumer_secret: 'kjU5dWLkxhwNw6AtHP2gNHuPus67mCuOaI1j8SFmZVFm0hMWox',
+    access_token: '2571622638-J4LLmwJMScRzaU8ztUakG92ZPS49i6FTqXgGQMJ',
+    access_token_secret: 'KjOyamQSfW75fygc89EGrU1JBiZYqIuhplCOxy3Zu8vQ4'
+});
+
+app.set("tweet", str);
+
+//twit.stream('statuses/filter', {
+//        'track': '#testingsomething123'
+//    }, function (stream) {
+//        stream.on('tweet', function (tweet) {
+//            console.log(data.user.name + "\n" + data.text);
+//            //str = data.user.name + "\n" + data.text;
+//            str = "" + data.text;
+//            var temp = "";
+//            for(var i = 0; i < str.length; i++){
+//                if(str.charAt(i) === "#") break;
+//                temp = temp + str.charAt(i);
+//            }
+//            str = temp;
+//            console.log("before query " + str);
+//            Wolfram.query(str, function(err, result) {
+//                if(err)
+//                    console.log(err);
+//                else
+//                {        
+//                    console.log("before store " + str);
+//                    str = result.queryresult.pod[1].subpod[0].plaintext[0];
+//                    console.log(str);
+//                    
+//                }
+//            });
+//        });
+//    });
+
+var stream = twit.stream('statuses/filter', {
+    'track': '#testingsomething123'
+});
+stream.on('tweet', function (tweet) {
+    str = "" + tweet.text;
+    var temp = "";
+    for (var i = 0; i < str.length; i++) {
+        if (str.charAt(i) === "#") break;
+        temp = temp + str.charAt(i);
+    }
+    str = temp;
+    console.log("before query " + str);
+    Wolfram.query(str, function (err, result) {
+        if (err)
+            console.log(err);
+        else {
+            console.log("before store " + str);
+            str = result.queryresult.pod[1].subpod[0].plaintext[0];
+            console.log(str);
+            str = str + " @" + tweet.user.screen_name;
+            twit.post('statuses/update', {
+                status: str
+            }, function (err, data, response) {
+                console.log(data);
+                console.log("did it work?");
+            });
+
+        }
     });
+    //    str = str + " @" + tweet.user.name;
+    //    twit.post('statuses/update', { status: 'Hello World?'}, function(err, data, response) {
+    //        console.log(data);
+    //        console.log("did it work?");
+    //    });
 
-twit.stream('statuses/filter', {
-        'track': '#testingsomething123'
-    }, function (stream) {
-        stream.on('data', function (data) {
-            console.log(data.user.name + "\n" + data.text);
-            str = data.user.name + "\n" + data.text;
-        });
-    });
+});
+
+//twit.post('statuses/update', { status: 'hello world!' }, function(err, data, response) {
+//  console.log(data)
+//});
 
 app.use(logfmt.requestLogger());
 
-app.set("tweet", str);
+
 
 app.all('/', function (req, res) {
     res.send(str);
