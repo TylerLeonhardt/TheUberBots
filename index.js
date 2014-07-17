6f8724368a0b91b9ded71f57a5bb48187d306e7d
tylerl0706 <tleonhardt@Tylers-MacBook-Pro.local>
2014-07-16 22:27:50 -0700

    readded express and logger

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
+/****************************************************/
+/*                     DISPLAY                      */
+/****************************************************/
+
+var express = require("express");
+var logfmt = require("logfmt");
+var app = express();
+
+app.use(logfmt.requestLogger());
+
+app.get('/', function(req, res) {
+  res.send('Hello World!');
+});
+
+var port = Number(process.env.PORT || 5000);
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
+
+/****************************************************/
+/*                     TWITTER                      */
+/****************************************************/
+
 var newrelic = require('newrelic');  //require for pinging
 var twit = require('twit');          //Twitter require
 
